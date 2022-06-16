6d85aae1ffae573b310355823236d79e334ea5bc
TaoXin <taoxincn@qq.com>
2022-06-16 15:12:52 +0800

    update

--- zlm/docker-entrypoint.sh
+++ zlm/docker-entrypoint.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 sed -i "s/api-secret/${SECRET}/g" /opt/media/conf/config.ini
+sed -i "s/DefaultMediaServer/${SERVERID}/g" /opt/media/conf/config.ini
+sed -i "s/8088/${PORT}/g" /opt/media/conf/config.ini
 exec "$@"
